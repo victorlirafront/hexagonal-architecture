a4d4e35339d1f225e9017e69064c80341aeb417d
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-31 17:46:56 -0300

    chore: add comments

--- module-2/structs/main.go
+++ module-2/structs/main.go
@@ -20,5 +20,7 @@ func main() {
 }
 
 // A struct em Go se assemelha ao type ou interface em TypeScript.
-
-// A utilidade de uma struct em Go (como a User no seu exemplo) é definir um tipo de dado composto que pode armazenar diferentes informações relacionadas. As structs permitem agrupar variáveis (campos) de tipos diferentes sob um único nome, o que facilita a organização, manipulação e passagem de dados entre funções. No seu caso, a struct User armazena informações sobre um usuário, como nome, sobrenome e idade.
+// A utilidade de uma struct em Go (como a User no seu exemplo) é definir um tipo de dado composto que pode armazenar
+// diferentes informações relacionadas. As structs permitem agrupar variáveis (campos) de tipos diferentes sob um único nome,
+//o que facilita a organização, manipulação e passagem de dados entre funções. No seu caso, a struct User armazena informações sobre um
+//usuário, como nome, sobrenome e idade.
